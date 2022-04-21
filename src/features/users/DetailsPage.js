6c64dc9254ab292a43c4249377adac18d4a79c36
Richard Almonte <richard508@users.noreply.github.com>
2022-04-21 14:23:17 -0400

    Build details page with modal

--- src/features/users/DetailsPage.js
+++ src/features/users/DetailsPage.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React from "react"
+import { Button, Modal, Row, Space, Table } from "antd"
 
-const UserDetailsPage = () => {
+const UserDetailsPage = ({ data }) => {
+  const [openModal, setOpenModal] = React.useState(false)
+  const [userData, setUserData] = React.useState(null)
+  const handleCancel = () => setOpenModal(false)
+  const handleOk = () => setOpenModal(false)
+  const viewProfile = (record) => {
+    setUserData(record)
+    setOpenModal(true)
+  }
+  const columns = [
+    {
+      title: "Company",
+      dataIndex: "company",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Position",
+      dataIndex: "position",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Button onClick={() => viewProfile(record)}>View Profile</Button>
+        </Space>
+      ),
+    },
+  ]
   return (
-    <div>
-      something
-    </div>
+    <>
+      <Table columns={columns} dataSource={data} />
+      <Modal title={`${userData?.name}'s Profile'`} visible={openModal} onOk={handleOk} onCancel={handleCancel}>
+        <Row>
+          Age: {userData?.profile.age}
+        </Row>
+        <Row>
+          Gender: {userData?.profile.gender}
+        </Row>
+        <Row>
+          Planet: {userData?.profile.planet}
+        </Row>
+        <Row>
+          Species: {userData?.profile.species}
+        </Row>
+        <Row>
+          Status: {userData?.profile.status}
+        </Row>
+      </Modal>
+    </>
   )
 }
 
