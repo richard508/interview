fbc558b786bd1a12b0bf4e53066ae6398391bc6d
Richard Almonte <richard508@users.noreply.github.com>
2022-04-22 11:08:45 -0400

    Add a route

--- src/Route.js
+++ src/Route.js
@@ -1,9 +1,9 @@
 import { Route, Switch } from 'react-router-dom'
 import UserPage from './features/users/UserPage'
 export const Routes = () => (
-    <Switch>
-        <Route exact path="/" component={UserPage} />
-    </Switch>
-  )
+  <Switch>
+    <Route exact path="/" component={UserPage} />
+  </Switch>
+)
   
-  export default Routes
+export default Routes
